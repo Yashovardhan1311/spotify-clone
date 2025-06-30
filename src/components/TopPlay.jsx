ddc511b9a847314f60aa21e4b6db6d74a43a6757
Yashovardhan1311 <yashvardhan.pratap@gmail.com>
2025-06-30 15:11:05 +0530

    Create TopPlay.jsx

---
+++ src/components/TopPlay.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const TopPlay = () => (
+  <div className="text-white p-4">
+    <h2 className="text-xl font-bold mb-2">Top Charts</h2>
+    <p>Top trending songs will be shown here.</p>
+  </div>
+);
+
+export default TopPlay;
