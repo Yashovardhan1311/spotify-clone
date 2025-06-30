4b5f488d51f3136e2aaf97ce8d0b71b5f41ca652
Yashovardhan1311 <yashvardhan.pratap@gmail.com>
2025-06-30 15:23:06 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        slideup: 'slideup 0.5s ease-in-out',
+      },
+      keyframes: {
+        slideup: {
+          '0%': { transform: 'translateY(100%)' },
+          '100%': { transform: 'translateY(0%)' },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
