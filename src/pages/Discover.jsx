28632b61023047356c953afb7b61e3189178ea8f
Yashovardhan1311 <yashvardhan.pratap@gmail.com>
2025-06-30 15:12:22 +0530

    Create Discover.jsx

---
+++ src/pages/Discover.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Discover = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-4">Discover</h1>
+      <p>Explore top music around the world!</p>
+    </div>
+  );
+};
+
+export default Discover;
